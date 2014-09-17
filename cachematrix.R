882b6df94e271f9a2eb6e2c394ab3c459dc09be8
Joe Schafer <github@thebitfarm.org>
2014-09-16 21:08:07 -0500

    CacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A helper function for caching matrix inversion calculations
+## and the solver invoking the wrapped matrix cache
 
-## Write a short comment describing this function
-
+## Create a wrapped matrix with caching
+## x : matrix (numeric)
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list( set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Solve a matrix inverse using a caching wrapper
+## x : makeCacheMatrix type
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
